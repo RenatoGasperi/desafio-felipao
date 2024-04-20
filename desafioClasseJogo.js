5c8f9094a94cbf7fcdce042dd6a257c11130e8b1
RenatoGasperi <99595341+RenatoGasperi@users.noreply.github.com>
2024-04-20 16:05:03 -0300

    Update desafioClasseJogo.js

--- desafioClasseJogo.js
+++ desafioClasseJogo.js
@@ -1,1 +1,36 @@
+class heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+        }
+    
+    exibirMensagem() {
+            switch (this.tipo) {
+                case "mago":
+                    var ataque = "magia"
+                    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+                    break;
+                case "guerreiro":
+                    var ataque = "espada"
+                    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+                    break;
+                case "monge":
+                    var ataque = "artes marciais"
+                    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+                    break;
+                case "monge":
+                    var ataque = "shuriken"
+                    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+                    break;
+                default:
+                    break;
+                
+            }
+            
+        }
+}
 
+primeiroHeroi = new heroi("renato", 40, "guerreiro")
+
+primeiroHeroi.exibirMensagem()
